559f7e82363359fc72e56082c51907d67d8cfc50
aezzahir <aero.engineer1337@gmail.com>
2023-12-25 04:20:01 -0800

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# file containing I love Puppet
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
