95e40ff7b76b9c222acfeff6b28787fd1b415a5a
aezzahir <aero.glowsky@gmail.com>
2024-02-14 11:51:28 +0100

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0.
-Hint: No authentication is necessary for most features of the Reddit API.
-If you’re getting errors related to Too Many Requests, ensure you’re setting
-a custom User-Agent.
-
-Requirements:
-
 Prototype: def number_of_subscribers(subreddit)
-If not a valid subreddit, return 0.
-NOTE: Invalid subreddits may return a redirect to search results.
-Ensure that you are not following redirects.
+- If not a valid subreddit, return 0.
+- NOTE: Invalid subreddits may return a redirect
+to search results. Ensure that you are not following redirects.
 """
 
 import requests
@@ -20,7 +11,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-        number_of_subscribers:
+    Retrieve the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers if successful, otherwise 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
